c075a06a6ac224e5e6fc03864802375bc6be9c08
happyeric1120 <happyeric1120@gmail.com>
2014-10-24 14:35:44 -0400

    add checkInMat into cacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -15,14 +15,45 @@ makeCacheMatrix <- function(x = matrix()) {
     
     # Check inverse matrix function
     checkInvMat <- function() {
-        # Check the dimension of x and its inverse matrix
-        if (nrow(x) != nrow(invMat) || ncol(x) != ncol(invMat)) {
-            return(FALSE)
+        if (!is.null(invMat)) {
+            # Check the dimension of x and its inverse matrix
+            if (nrow(x) != nrow(invMat) || ncol(x) != ncol(invMat)) {
+                message("The inverse matrix is incorrect, please run cacheSolve to update!")
+                return(FALSE)
+            }
+            # Check current inverse matrix is true inverse matrix for the matrix
+            if (checkIdentityMatrix(x%*%invMat)) {
+                message("The inverse matrix is correct, no update needed!")
+                return(TRUE)
+            } else {
+                message("The inverse matrix is incorrect, please run cacheSolve to update!")
+                return(FALSE)
+            }
         }
+        message("The inverse matrix is missing, please run cacheSolve to update!")
+        return(FALSE)
+
+        
+    }
+    # This function is to check if a matrix is a identity matrix.
+    checkIdentityMatrix <- function(matrix) {
+        boolean = FALSE
+        for (i in seq_len(nrow(matrix))) {
+            for (j in seq_len(ncol(matrix))) {
+                
+                if (i == j && matrix[i,j] !=1)
+                    return(boolean)
+                if (i!=j && matrix[i,j] !=0)
+                    return(boolean)
+            }
+        }
+        boolean = TRUE
+        return(boolean)
     }
     list(set = set, get = get,
          setInvMat = setInvMat,
-         getInvMat = getInvMat)
+         getInvMat = getInvMat,
+         checkInvMat = checkInvMat)
 }
 
 
@@ -54,33 +85,21 @@ cacheSolve <- function(x, ...) {
     invMat 
 }
 
-
+# The belows are the testing codes
 a <- rbind(c(4,3), c(3,2))
 testMat <- makeCacheMatrix(a)
-
 cacheSolve(testMat)
 
-c <- rbind(c(1,0), c(0,2))
+# Show and check inverse matrix
+print("The matrix is:")
+print(testMat$get())
+print("Its inverse matrix is")
+print(testMat$getInvMat())
+testMat$checkInvMat()
 
-checkIdentityMatrix <- function(matrix) {
-    boolean = FALSE
-    for (i in seq_len(nrow(matrix))) {
-        for (j in seq_len(ncol(matrix))) {
-            print(matrix[i,j])
-            
-            if (i == j && matrix[i,j] !=1)
-                return(boolean)
-            if (i!=j && matrix[i,j] !=0)
-                return(boolean)
-        }
-    }
-    boolean = TRUE
-    return(boolean)
-}
+b <- rbind(c(1,2,3), c(0,1,4), c(5,6,0))
+testMat$set(b)
+testMat$checkInvMat()
 
-if (checkIdentityMatrix(c)){
-    print("OK")
-} else {
-    print("NO")
-}
+
     
